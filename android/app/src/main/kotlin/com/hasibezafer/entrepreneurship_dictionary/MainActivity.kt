af2b392d68a6fcf2ef345772d2d5bf52049c9aab
Hasibe <hasibe.zafer@ogr.iu.edu.tr>
2022-12-16 22:06:10 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/hasibezafer/entrepreneurship_dictionary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hasibezafer.entrepreneurship_dictionary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/constant.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+// Colors
+const Color kColorMainApp = Color.fromARGB(255, 18, 103, 252);
+const Color kColorButton = Color.fromARGB(255, 86, 86, 246);
+const Color kColorText = Color.fromARGB(255, 51, 51, 51);
+
+//Icon Paths
+const String kIconAppLogo = 'assets/icons/entrepreneurship_logo.png';
+const String kIconGoogleSignIn = 'assets/icons/ic_google_sign_in_button.png';
+
+// Size
+const double kFontSizeTitleText= 32.0;
+const double kSizeHeightIconGoogleSignIn= 32.0;
+const double kSizeHeightLoginIcon= 32.0;
+const double kSizeHeightAppIcon= 200.0;
+const double kSizeWidthAppIcon= 200.0;
+
